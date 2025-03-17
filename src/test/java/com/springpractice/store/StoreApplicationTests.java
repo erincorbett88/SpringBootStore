5687b51a8fd2a2b2c853480900483018c0a4b70d
Erin Corbett <ecorbett.ctr@kr.af.mil>
2025-03-17 08:28:17 -0600

    first commit

---
+++ src/test/java/com/springpractice/store/StoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springpractice.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
