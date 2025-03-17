e0453dde98a88ba5b6c212fa03ede5f925311a43
Erin Corbett <erin.e.corbett@gmail.com>
2025-03-17 14:29:58 -0600

    added value variable

--- src/main/java/com/springpractice/store/HomeController.java
+++ src/main/java/com/springpractice/store/HomeController.java
@@ -1,17 +1,21 @@
 package com.springpractice.store;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 //an annotation is a tag that we add in the code to give instructions to the controller
 //can be added to methods, fields, parameters...
-//this tells spring that the class shoudl be used as a Web Controller for receiving web traffic
+//this tells spring that the class should be used as a Web Controller for receiving web traffic
 public class HomeController {
+    @Value("${spring.application.name}")
+    private String name;
 
     @RequestMapping("/")
     //("/") is the URL that the controller will be mapped to - the root of our website
     public String index(){
+        System.out.println("appName: " + name);
         return "index.html";
         //this method returns a string that is the name of the template file that should be used to render the response
         //in other words, this is the name of the view that should be returned to the browser
