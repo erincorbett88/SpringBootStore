b173031ad3300ab0cd6ea31060534ebd45c009ae
Erin Corbett <erin.e.corbett@gmail.com>
2025-04-14 08:14:49 -0600

    add db

--- src/main/java/com/springpractice/store/StoreApplication.java
+++ src/main/java/com/springpractice/store/StoreApplication.java
@@ -12,13 +12,5 @@ public class StoreApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(StoreApplication.class, args);
 
-        var userService = context.getBean("userService", UserService.class);
-        userService.registerUser(new User(654123, "erin.corbett@gmail.com", "Erin Corbett", "asdflkj12"));
-//
-//        var orderService = context.getBean("orderService", OrderService.class);
-//        var orderService2 = context.getBean("orderService", OrderService.class);
-//        orderService.placeOrder();
-//        orderService2.placeOrder();
-
     }
 }
