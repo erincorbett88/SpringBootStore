d765d36cdf1cceffe66031fe758c74670b48fd43
Erin Corbett <erin.e.corbett@gmail.com>
2025-04-24 08:26:54 -0600

    add tags table

--- src/main/resources/db/migration/V4__add_profiles_table.sql
+++ src/main/resources/db/migration/V4__add_profiles_table.sql
@@ -5,5 +5,5 @@ create table profiles
     phone_number   VARCHAR(15),
     date_of_birth  DATE,
     loyalty_points int unsigned default 0,
-    FOREIGN KEY (id) REFERENCES users(id),
+    FOREIGN KEY (id) REFERENCES users(id)
 );
